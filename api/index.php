134f2d08247a5dc675900b636de87e8fd5e96bb9
LucasOliveiraEvangelista <lucas.evangelistadev@gmail.com>
2021-12-02 09:32:39 -0300

    teste php

---
+++ api/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Ponto || trial</title>
+
+    <meta http-equiv="refresh" content="5" />
+</head>
+<body>
+    <h1>O começo de tudo</h1>
+    <?php 
+    
+    echo "hello world";
+
+    ?>
+</body>
+</html>
